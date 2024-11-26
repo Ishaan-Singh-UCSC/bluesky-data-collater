d1618f6c6c09ab0f3ffa1f9c531543f979a7a237
Nicky <nschlege@ucsc.edu>
2024-11-25 22:02:48 -0800

    comments

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from src.BlueSkyTweetExtractor import BlueskyTweetExtractor
-from src.Tokenizer import BlueSkyTokenizer
+# from src.Tokenizer import BlueSkyTokenizer
+# from src.BERTTextSummarization import TextSummarizer
+from src.KeywordExtraction import KeywordExtractor
 
-NUM_TWEETS = 10
-
+# Number of tweets to find
+NUM_TWEETS = 1000
+NUMCORES = 1
 def main():
+    '''Gets tweets from Bluesky based on a set variable, and finds the keywords to print'''
+    print("Getting", NUM_TWEETS, "tweets")
     creds = open("creds.txt", "r") 
     line = creds.readline()
     user_pass = line.split(",")
@@ -12,16 +17,26 @@ def main():
     newExtractor = BlueskyTweetExtractor(user_pass[0], user_pass[1])
 
     data = newExtractor.get_tweets(NUM_TWEETS)
+    string_data = ''
     for item in data:
-        print(item,"\n")
-
-    newTokenizer = BlueSkyTokenizer()
-    tokenized_data = newTokenizer.convertToTokens_User_Tweet_Tuple(data)
-    for item in tokenized_data:
-        print(item, "\n")
+        string_data += item[1]
+        # print(item,"\n")
     
-
-
+    newKeywordExtractor = KeywordExtractor()
+    keywords = newKeywordExtractor.keyword_extraction(string_data)
+    for item in keywords:
+        print (item)
+    
+    # print("Found tweets: ", len(data))
+    # Tokenizer, not used
+    # newTokenizer = BlueSkyTokenizer()
+    # tokenized_data = newTokenizer.convertToTokens_User_Tweet_Tuple(data)
+    # for item in tokenized_data:
+    #     print(item, "\n")
+    
+    # Summarizer, not used
+    # newSummarizer = TextSummarizer()
+    # summaries = newSummarizer.summarize_Tweets(data, NUMCORES)
 
 if __name__ == '__main__':
     main()
