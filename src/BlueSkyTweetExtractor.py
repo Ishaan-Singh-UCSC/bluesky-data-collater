70bfa14f40b0819c4a6fe07e269b42de0d4cbad1
Nicky <nschlege@ucsc.edu>
2024-11-25 21:59:00 -0800

    comments

--- src/BlueSkyTweetExtractor.py
+++ src/BlueSkyTweetExtractor.py
@@ -1,31 +1,42 @@
-'''Contains functions in order to extract tweets from BlueSky.
-@Params: TODO
-@Return: TODO'''
-
 from atproto import Client
 from math import floor
 
 class BlueskyTweetExtractor:
+    '''Use this class to extract tweets from bluesky'''
     def __init__(self, email, password):
+        '''Creates the BlueskyTweetExtractor object, usuing atproto api.
+        Takes an email and password to login'''
         self.client = Client()
         self.profile = self.client.login(email, password)
         print('Successfully logged in as:', self.profile.display_name)
-        return
 
-    def get_tweets(self, batchsize):
-        '''Uses bluesky api to get tweets in iterations and batch sizes, moving a cursor to get new tweets.'''
+    def get_tweets(self, batchsize=10):
+        '''Uses bluesky api to get tweets in iterations and batch sizes, moving a cursor to get new tweets.
+        Takes a batchsize int. Return in the form of a list of tuples ("{Posting User}", "{Tweet content}")'''
         tweets = []
+        # Invalid input will return 10 tweets
+        if batchsize < 1:
+            batchsize = 10
+        # Max limit size is 100, so it determines how many to take
         num_iters = floor(batchsize/100)
         first_iter = batchsize % 100
+
+        # These two blocks will loop through and get the desired amount of tweets according to batchsize
+        # When the amount of tweets to get is divisible by 100
         if first_iter == 0:
+            # Retreives the data of all the tweets
             data = self.client.get_timeline(limit=100)
+            # Generator object for all the tweets found
             feed = data.feed
+            # Nextpage tracks where to start when we continue to gather tweets
             nextpage = data.cursor
+            # Extracts data in a loop
             for tweet in feed:
                 # Removes reply tweets
                 #if tweet.post.record.reply == None: 
                 tweets.append((tweet.post.author.handle, tweet.post.record.text))
-            apiAccessLoop(self.client, num_iters - 1, tweets, nextpage)
+            __apiAccessLoop__(self.client, num_iters - 1, tweets, nextpage)
+        # When the amount of tweets to get is not divisible by 100
         else:
             data = self.client.get_timeline(limit=first_iter)
             feed = data.feed
@@ -34,8 +45,7 @@ class BlueskyTweetExtractor:
                 # Removes reply tweets
                 # if tweet.post.record.reply == None:
                 tweets.append((tweet.post.author.handle, tweet.post.record.text))
-            apiAccessLoop(self.client, num_iters, tweets, nextpage)
-
+            __apiAccessLoop__(self.client, num_iters, tweets, nextpage)
         return tweets
     
     def get_training_tweets(self):
@@ -49,8 +59,8 @@ class BlueskyTweetExtractor:
         return tweets
 
 
-def apiAccessLoop(client, num_iters, tweets, nextpage):
-    
+def __apiAccessLoop__(client, num_iters, tweets, nextpage):
+    '''Loop of repeating code to simplify process. not meant to be called by the user'''
     for i in range(num_iters):
         data = client.get_timeline(cursor=nextpage,limit=100)
         feed = data.feed
