f66a50c597cc5ed02e5e14d4d56f45974a86a0ba
Nicky <nschlege@ucsc.edu>
2024-11-25 18:17:06 -0800

    created tokenizzer

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from src.BlueSkyTweetExtractor import BlueskyTweetExtractor
+from src.Tokenizer import BlueSkyTokenizer
 
-creds = open("creds.txt", "r") 
-line = creds.readline()
-user_pass = line.split(",")
-creds.close()
+NUM_TWEETS = 10
 
-newExtractor = BlueskyTweetExtractor(user_pass[0], user_pass[1])
+def main():
+    creds = open("creds.txt", "r") 
+    line = creds.readline()
+    user_pass = line.split(",")
+    creds.close()
 
-print(newExtractor.get_tweets(30))
+    newExtractor = BlueskyTweetExtractor(user_pass[0], user_pass[1])
+
+    data = newExtractor.get_tweets(NUM_TWEETS)
+    for item in data:
+        print(item,"\n")
+
+    newTokenizer = BlueSkyTokenizer()
+    tokenized_data = newTokenizer.convertToTokens_User_Tweet_Tuple(data)
+    for item in tokenized_data:
+        print(item, "\n")
+    
+
+
+
+if __name__ == '__main__':
+    main()
