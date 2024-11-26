579976af18c48c9d9891c398e073929d3fd87679
Nicky <nschlege@ucsc.edu>
2024-11-26 00:46:50 -0800

    parser for json files, two functions one per style, untested

--- src/JSONParser.py
+++ src/JSONParser.py
@@ -1,3 +1,37 @@
+import json
+
+class JSONParser:
+    def __init__(self):
+        self.tweet_tuples = []
+        self.large_string = ''
+
+    def parse_json_file_with_path(self, path):
+        '''Parses JSON files, adding them to a tweet tuple as normal,
+        also adds them to a string
+        Returns 1 on failure, 0 on success'''
+        with open(path, 'r') as file:
+            json_data = json.load(file)
+
+        jsonposts = json_data["posts"]
+        if jsonposts == None:
+            print("ERROR: JSON does not contain posts")
+            return 1
+        
+        for item in jsonposts:
+            self.large_string += " " + item["record"]["text"]
+            self.tweet_tuples.append((item["author"]["handle"], item["record"]["text"]))
 
 
-class JSONParser
+    def parse_json_already_loaded(self, json):
+        '''Parses a JSON, adding them to a tweet tuple as normal,
+        also adds them to a string
+        Returns 1 on failure, 0 on success'''
+
+        jsonposts = json["posts"]
+        if jsonposts == None:
+            print("ERROR: JSON does not contain posts")
+            return 1
+        
+        for item in jsonposts:
+            self.large_string += " " + item["record"]["text"]
+            self.tweet_tuples.append((item["author"]["handle"], item["record"]["text"]))
